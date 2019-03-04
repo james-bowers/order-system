a5a30bd9359d7e8af6adc763437c9112ef9632f5
James Bowers <james@james-bowers.me>
2019-03-04 06:29:59 +0000

    handle failure gently in payout model

--- lib/order_system/model/payout_model.ex
+++ lib/order_system/model/payout_model.ex
@@ -3,18 +3,26 @@ defmodule OrderSystem.PayoutModel do
 
   def create_payout(%{amount: _, account_id: _, stripe_transfer_id: _} = attrs) do
     Repo.transaction(fn ->
-      transfer_attrs = insert_transfer!(attrs)
-
-      %Payout{}
-      |> Payout.changeset(transfer_attrs)
-      |> Repo.insert!()
+      with {:ok, transfer_attrs} <- insert_transfer(attrs),
+      {:ok, payout} <- insert_payout(transfer_attrs) do
+        payout
+      else
+        {:error, error} -> Repo.rollback(error)
+      end
     end)
   end
 
-  defp insert_transfer!(transfer_attrs) do
-    {:ok, transfer} = TransferModel.create_transfer(transfer_attrs)
+  defp insert_payout(transfer_attrs) do
+    %Payout{}
+    |> Payout.changeset(transfer_attrs)
+    |> Repo.insert()
+  end
 
-    transfer_attrs
-    |> Map.put(:transfer_id, transfer.id)
+  defp insert_transfer(transfer_attrs) do
+    with {:ok, transfer} <- TransferModel.create_transfer(transfer_attrs) do
+      {:ok, Map.put(transfer_attrs, :transfer_id, transfer.id)}
+    else
+      {:error, changeset} -> {:error, changeset}
+    end
   end
 end
